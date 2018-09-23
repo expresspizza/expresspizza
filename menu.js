5d480e2c4289b06c8babfb130a8e22ad32f22b33
jaffersyed26 <35580379+jaffersyed26@users.noreply.github.com>
2018-09-23 14:57:20 -0400

    update files

---
+++ menu.js
@@ -0,0 +1,327 @@
+function specials() {
+    var empty = document.querySelector('.empty-menu');
+    var pizzaShow = document.querySelector('.pizza');
+    var calzoneShow = document.querySelector('.calzone');
+    var pita = document.querySelector('.pita-wraps');
+    var phillies = document.querySelector('.xpress-phillies');
+    var burgers = document.querySelector('.burgers');
+    var submarines = document.querySelector('.submarines');
+    var salads = document.querySelector('.salads');
+    var beverages = document.querySelector('.beverages');
+    var desserts = document.querySelector('.desserts');
+    var sideorders = document.querySelector('.sideOrders');
+    empty.style.display = 'block';
+    pizzaShow.style.display = 'none';
+    calzoneShow.style.display = 'none';
+    pita.style.display = 'none';
+    phillies.style.display = 'none';
+    burgers.style.display = 'none';
+    submarines.style.display = 'none';
+    salads.style.display = 'none';
+    sideorders.style.display = 'none';
+    beverages.style.display = 'none';
+    desserts.style.display = 'none';
+}
+
+function pizza(){
+    var empty = document.querySelector('.empty-menu');
+    var pizzaShow = document.querySelector('.pizza');
+    var calzoneShow = document.querySelector('.calzone');
+    var pita = document.querySelector('.pita-wraps');
+    var phillies = document.querySelector('.xpress-phillies');
+    var burgers = document.querySelector('.burgers');
+    var submarines = document.querySelector('.submarines');
+    var salads = document.querySelector('.salads');
+    var beverages = document.querySelector('.beverages');
+    var desserts = document.querySelector('.desserts');
+    var sideorders = document.querySelector('.sideOrders');
+    empty.style.display = 'none';
+    pizzaShow.style.display = 'block';
+    calzoneShow.style.display = 'none';
+    pita.style.display = 'none';
+    phillies.style.display = 'none';
+    burgers.style.display = 'none';
+    submarines.style.display = 'none';
+    salads.style.display = 'none';
+    sideorders.style.display = 'none';
+    beverages.style.display = 'none';
+    desserts.style.display = 'none';
+}
+
+function calzone(){
+    var calzoneShow = document.querySelector('.calzone');
+    var pizzaShow = document.querySelector('.pizza');
+    var pita = document.querySelector('.pita-wraps');
+    var empty = document.querySelector('.empty-menu');
+    var wings = document.querySelector('.wings');
+    var phillies = document.querySelector('.xpress-phillies');
+    var burgers = document.querySelector('.burgers');
+    var submarines = document.querySelector('.submarines');
+    var salads = document.querySelector('.salads');
+    var beverages = document.querySelector('.beverages');
+    var desserts = document.querySelector('.desserts');
+    var sideorders = document.querySelector('.sideOrders');
+    empty.style.display = 'none';
+    calzoneShow.style.display = 'block';
+    pizzaShow.style.display = 'none'
+    pita.style.display = 'none';
+    wings.style.display = 'none';
+    phillies.style.display = 'none';
+    burgers.style.display = 'none';
+    submarines.style.display = 'none';
+    salads.style.display = 'none';
+    sideorders.style.display = 'none';
+    beverages.style.display = 'none';
+    desserts.style.display = 'none';
+}
+
+function wings() {
+    var pita = document.querySelector('.pita-wraps');
+    var calzoneShow = document.querySelector('.calzone');
+    var pizzaShow = document.querySelector('.pizza');
+    var wings = document.querySelector('.wings');
+    var empty = document.querySelector('.empty-menu');
+    var phillies = document.querySelector('.xpress-phillies');
+    var burgers = document.querySelector('.burgers');
+    var submarines = document.querySelector('.submarines');
+    var salads = document.querySelector('.salads');
+    var beverages = document.querySelector('.beverages');
+    var desserts = document.querySelector('.desserts');
+    var sideorders = document.querySelector('.sideOrders');
+    empty.style.display = 'none';
+    pita.style.display = 'none';
+    calzoneShow.style.display = 'none';
+    pizzaShow.style.display = 'none';
+    wings.style.display = 'block';
+    phillies.style.display = 'none';  
+    burgers.style.display = 'none';
+    submarines.style.display = 'none';
+    salads.style.display = 'none';
+    sideorders.style.display = 'none';
+    beverages.style.display = 'none';
+    desserts.style.display = 'none';
+}
+
+function phillies() {
+    var phillies = document.querySelector('.xpress-phillies');
+    var pita = document.querySelector('.pita-wraps');
+    var calzoneShow = document.querySelector('.calzone');
+    var pizzaShow = document.querySelector('.pizza');
+    var wings = document.querySelector('.wings');
+    var empty = document.querySelector('.empty-menu');
+    var burgers = document.querySelector('.burgers');
+    var submarines = document.querySelector('.submarines');
+    var salads = document.querySelector('.salads');
+    var beverages = document.querySelector('.beverages');
+    var desserts = document.querySelector('.desserts');
+    var sideorders = document.querySelector('.sideOrders');
+    empty.style.display = 'none';
+    pita.style.display = 'none';
+    calzoneShow.style.display = 'none';
+    pizzaShow.style.display = 'none';
+    wings.style.display = 'none';
+    phillies.style.display = 'block';
+    burgers.style.display = 'none';
+    submarines.style.display = 'none';
+    salads.style.display = 'none';
+    sideorders.style.display = 'none';
+    beverages.style.display = 'none';
+    desserts.style.display = 'none';
+}
+
+function burgers() {
+    var pita = document.querySelector('.pita-wraps');
+    var calzoneShow = document.querySelector('.calzone');
+    var pizzaShow = document.querySelector('.pizza');
+    var empty = document.querySelector('.empty-menu');
+    var wings = document.querySelector('.wings');
+    var phillies = document.querySelector('.xpress-phillies');
+    var burgers = document.querySelector('.burgers');
+    var submarines = document.querySelector('.submarines');
+    var salads = document.querySelector('.salads');
+    var beverages = document.querySelector('.beverages');
+    var desserts = document.querySelector('.desserts');
+    var sideorders = document.querySelector('.sideOrders');
+    empty.style.display = 'none';
+    pita.style.display = 'none';
+    calzoneShow.style.display = 'none';
+    pizzaShow.style.display = 'none';
+    wings.style.display = 'none';
+    phillies.style.display = 'none';
+    burgers.style.display = 'block';
+    submarines.style.display = 'none';
+    salads.style.display = 'none';
+    sideorders.style.display = 'none';
+    beverages.style.display = 'none';
+    desserts.style.display = 'none';
+}
+
+function submarines() {
+    var pita = document.querySelector('.pita-wraps');
+    var calzoneShow = document.querySelector('.calzone');
+    var pizzaShow = document.querySelector('.pizza');
+    var empty = document.querySelector('.empty-menu');
+    var wings = document.querySelector('.wings');
+    var phillies = document.querySelector('.xpress-phillies');
+    var burgers = document.querySelector('.burgers');
+    var submarines = document.querySelector('.submarines');
+    var salads = document.querySelector('.salads');
+    var beverages = document.querySelector('.beverages');
+    var desserts = document.querySelector('.desserts');
+    var sideorders = document.querySelector('.sideOrders');
+    empty.style.display = 'none';
+    pita.style.display = 'none';
+    calzoneShow.style.display = 'none';
+    pizzaShow.style.display = 'none';
+    wings.style.display = 'none';
+    phillies.style.display = 'none';
+    burgers.style.display = 'none';
+    submarines.style.display = 'block';
+    salads.style.display = 'none';
+    sideorders.style.display = 'none';
+    beverages.style.display = 'none';
+    desserts.style.display = 'none';
+}
+
+function pita() {
+    var pita = document.querySelector('.pita-wraps');
+    var calzoneShow = document.querySelector('.calzone');
+    var pizzaShow = document.querySelector('.pizza');
+    var empty = document.querySelector('.empty-menu');
+    var wings = document.querySelector('.wings');
+    var phillies = document.querySelector('.xpress-phillies');
+    var burgers = document.querySelector('.burgers');
+    var submarines = document.querySelector('.submarines');
+    var salads = document.querySelector('.salads');
+    var beverages = document.querySelector('.beverages');
+    var desserts = document.querySelector('.desserts');
+    var sideorders = document.querySelector('.sideOrders');
+    empty.style.display = 'none';
+    pita.style.display = 'block';
+    calzoneShow.style.display = 'none';
+    pizzaShow.style.display = 'none';
+    wings.style.display = 'none';
+    phillies.style.display = 'none';
+    burgers.style.display = 'none';
+    submarines.style.display = 'none';
+    salads.style.display = 'none';
+    sideorders.style.display = 'none';
+    beverages.style.display = 'none';
+    desserts.style.display = 'none';
+}
+
+function salads() {
+    var pita = document.querySelector('.pita-wraps');
+    var calzoneShow = document.querySelector('.calzone');
+    var pizzaShow = document.querySelector('.pizza');
+    var empty = document.querySelector('.empty-menu');
+    var wings = document.querySelector('.wings');
+    var phillies = document.querySelector('.xpress-phillies');
+    var burgers = document.querySelector('.burgers');
+    var submarines = document.querySelector('.submarines');
+    var salads = document.querySelector('.salads');
+    var beverages = document.querySelector('.beverages');
+    var desserts = document.querySelector('.desserts');
+    var sideorders = document.querySelector('.sideOrders');
+    empty.style.display = 'none';
+    pita.style.display = 'none';
+    calzoneShow.style.display = 'none';
+    pizzaShow.style.display = 'none';
+    wings.style.display = 'none';
+    phillies.style.display = 'none';
+    burgers.style.display = 'none';
+    submarines.style.display = 'none';
+    salads.style.display = 'block';
+    sideorders.style.display = 'none';
+    beverages.style.display = 'none';
+    desserts.style.display = 'none';
+}
+
+function sideOrders() {
+    var pita = document.querySelector('.pita-wraps');
+    var calzoneShow = document.querySelector('.calzone');
+    var pizzaShow = document.querySelector('.pizza');
+    var empty = document.querySelector('.empty-menu');
+    var wings = document.querySelector('.wings');
+    var phillies = document.querySelector('.xpress-phillies');
+    var burgers = document.querySelector('.burgers');
+    var submarines = document.querySelector('.submarines');
+    var salads = document.querySelector('.salads');
+    var sideorders = document.querySelector('.sideOrders');
+    var beverages = document.querySelector('.beverages');
+    var desserts = document.querySelector('.desserts');
+    empty.style.display = 'none';
+    pita.style.display = 'none';
+    calzoneShow.style.display = 'none';
+    pizzaShow.style.display = 'none';
+    wings.style.display = 'none';
+    phillies.style.display = 'none';
+    burgers.style.display = 'none';
+    submarines.style.display = 'none';
+    salads.style.display = 'none';
+    sideorders.style.display = 'block';
+    beverages.style.display = 'none';
+    desserts.style.display = 'none';
+
+}
+
+function desserts() {
+    var pita = document.querySelector('.pita-wraps');
+    var calzoneShow = document.querySelector('.calzone');
+    var pizzaShow = document.querySelector('.pizza');
+    var empty = document.querySelector('.empty-menu');
+    var wings = document.querySelector('.wings');
+    var phillies = document.querySelector('.xpress-phillies');
+    var burgers = document.querySelector('.burgers');
+    var submarines = document.querySelector('.submarines');
+    var salads = document.querySelector('.salads');
+    var sideorders = document.querySelector('.sideOrders');
+    var beverages = document.querySelector('.beverages');
+    var desserts = document.querySelector('.desserts');
+    empty.style.display = 'none';
+    pita.style.display = 'none';
+    calzoneShow.style.display = 'none';
+    pizzaShow.style.display = 'none';
+    wings.style.display = 'none';
+    phillies.style.display = 'none';
+    burgers.style.display = 'none';
+    submarines.style.display = 'none';
+    salads.style.display = 'none';
+    sideorders.style.display = 'none';
+    beverages.style.display = 'none';
+    desserts.style.display = 'block';
+}
+
+function beverages() {
+    var pita = document.querySelector('.pita-wraps');
+    var calzoneShow = document.querySelector('.calzone');
+    var pizzaShow = document.querySelector('.pizza');
+    var empty = document.querySelector('.empty-menu');
+    var wings = document.querySelector('.wings');
+    var phillies = document.querySelector('.xpress-phillies');
+    var burgers = document.querySelector('.burgers');
+    var submarines = document.querySelector('.submarines');
+    var salads = document.querySelector('.salads');
+    var sideorders = document.querySelector('.sideOrders');
+    var beverages = document.querySelector('.beverages');
+    var desserts = document.querySelector('.desserts');
+    empty.style.display = 'none';
+    pita.style.display = 'none';
+    calzoneShow.style.display = 'none';
+    pizzaShow.style.display = 'none';
+    wings.style.display = 'none';
+    phillies.style.display = 'none';
+    burgers.style.display = 'none';
+    submarines.style.display = 'none';
+    salads.style.display = 'none';
+    sideorders.style.display = 'none';
+    beverages.style.display = 'block';
+    desserts.style.display = 'none';
+}
+
+
+
+
+
+
+
